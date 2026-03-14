280981a62215d4d2af4531b10ea08ead0e82505e
pratiksanap01 <sanappratik07@gmail.com>
2026-03-14 22:41:05 +0530

    New get req

--- Express/express_server.js
+++ Express/express_server.js
@@ -2,10 +2,15 @@
 
 import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello from express server")
+});
+
+
+app.get('/about', (req, res) =>{
+    res.send("You are on about page")
 });
 
 
