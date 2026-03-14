aedc423b17bc317d930312f55728e06d3e26e2dc
pratiksanap01 <sanappratik07@gmail.com>
2026-03-14 23:12:03 +0530

    New routes added

--- Express/express_server.js
+++ Express/express_server.js
@@ -1,17 +1,32 @@
 // First express server
+// Create a server with 
+// GET /
+// GET /about
+// GET /contact
+// POST /user
 
 import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello from express server")
 });
 
 
-app.get('/about', (req, res) =>{
+app.get("/about", (req, res) =>{
     res.send("You are on about page")
 });
+
+
+app.get("/contact", (req, res) => {
+    app.send("Contact Page")
+});
+
+
+app.post("/user", (req, res) => {
+    res.send("User created")
+})
 
 
 app.listen(3000,() => {
